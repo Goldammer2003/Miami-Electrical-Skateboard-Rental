26bd92d5133f6172f70bfa4a72c8f2f86bde04c7
Manuel Alexander Goldammer <mangobo@hotmail.de>
2022-11-28 00:04:47 +0000

    Setting up the navbar, first raw version with still existing errors to fix

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -2,18 +2,40 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar narbar-expand-lg bg-white py-3 fixed-top">
+      <div className="container">
+        <a href="#" className="navbar-brand text-dark">
+          Skateboard Rentals South Beach
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navmenu"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navmenu">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <a href="#learn" className="nav-link">
+                What You'll Learn
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#questions" className="nav-link">
+                Questions
+              </a>
+            </li>
+            <li clasName="nav-item">
+              <a href="#instructors" className="nav-link">
+                Instructors
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
