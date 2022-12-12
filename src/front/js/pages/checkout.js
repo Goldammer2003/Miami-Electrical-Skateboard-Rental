f9a05d96e66ead2c9640c9fe00d223375c6cf032
Manuel Alexander Goldammer <mangobo@hotmail.de>
2022-12-12 19:29:29 +0000

    Calender with checkout page working

--- src/front/js/pages/checkout.js
+++ src/front/js/pages/checkout.js
@@ -1,6 +1,21 @@
 import React from "react";
-
+import { useContext } from "react";
+import { Context } from "../store/appContext";
 const Checkout = () => {
-  return <div>checkout</div>;
+  const { store, actions } = useContext(Context);
+  return (
+    <div>
+      {store.cartList.map((items) => {
+        return (
+          <div>
+            <h1>My Bookings:</h1>
+            <h2>{items.name}</h2>
+            <h2>{items.skateboard_type}</h2>
+            <h2>{items.date}</h2>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 export default Checkout;
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,7 +1,7 @@
 const getState = ({ getStore, getActions, setStore }) => {
   return {
     store: {
-      isLogin: false,
+      isLogin: true,
       message: null,
       cartList: [],
       demo: [
