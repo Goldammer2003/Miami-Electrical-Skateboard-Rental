e38914fb12252b42e29d47fefe4a4a48a878cb60
Enmanuelph98 <enmanuelph98@gmail.com>
2022-12-13 00:57:55 +0000

    data base created

--- src/api/models.py
+++ src/api/models.py
@@ -5,8 +5,17 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    password = db.Column(db.String(20), unique=False, nullable=False)
+    userName= db.Column(db.String(25), nullable=False)
+    firstName= db.Column(db.String(25), nullable=False)
+    lastName= db.Column(db.String(25), nullable=False)
+    dob= db.Column(db.String(10), nullable=False)
+    address= db.Column(db.String(120), nullable=False)
+    address2= db.Column(db.String(120), nullable=False)
+    city= db.Column(db.String(25) , nullable=False)
+    state= db.Column(db.String(25) , nullable=False)
+    country= db.Column(db.String(25) , nullable=False)
+    zipcode= db.Column(db.String(100), nullable=False)
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -15,5 +24,19 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "firstName": self.firstName,
+            "lastName": self.lastName,
+            "dob": self.dob,
+            "address": self.address,
+            "address2" :self.address2,
+            "city": self.city,
+            "state": self.state,
+            "country": self.country,
+            "zipcode": self.zipcode,
+            "userName":self.userName,
+
+
+
+            
             # do not serialize the password, its a security breach
         }
