80d6825b410c9ad4e1b145c27d2c9de4f710eb53
JesseMiami100 <jessemetayer@gmail.com>
2022-12-14 00:30:37 +0000

    update

--- src/front/js/pages/aboutus.js
+++ src/front/js/pages/aboutus.js
@@ -24,13 +24,11 @@ const Aboutus = () => {
                 ></FontAwesomeIcon>
               </h6>
               <p className="card-text">
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
+                
               </p>
             </div>
             <div className="card-footer">
-              <small className="text-muted">Last updated 3 mins ago</small>
+              <small className="text-muted"></small>
             </div>
           </div>
         </div>
@@ -52,12 +50,11 @@ const Aboutus = () => {
                 ></FontAwesomeIcon>
               </h6>
               <p className="card-text">
-                This card has supporting text below as a natural lead-in to
-                additional content.
+                
               </p>
             </div>
             <div className="card-footer">
-              <small className="text-muted">Last updated 3 mins ago</small>
+              <small className="text-muted"></small>
             </div>
           </div>
         </div>
@@ -81,13 +78,11 @@ const Aboutus = () => {
                 </a>
               </h6>
               <p className="card-text">
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This card has even longer content
-                than the first to show that equal height action.
+                
               </p>
             </div>
             <div className="card-footer">
-              <small className="text-muted">Last updated 3 mins ago</small>
+              <small className="text-muted"></small>
             </div>
           </div>
         </div>
