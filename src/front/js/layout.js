1c0a88fb78c93065e4c9146a678fe17276cc6064
Manuel Alexander Goldammer <mangobo@hotmail.de>
2022-12-14 22:46:23 +0000

    completed the subpage/booking calender for rental option Nr.4

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -20,6 +20,8 @@ import { Detailedcardone } from "./pages/detailedcardone";
 import Checkout from "./pages/checkout";
 import { Detailedcardtwo } from "./pages/detailedcardtwo";
 import { Detailedcardthree } from "./pages/detailedcardthree";
+import { Detailedcardfour } from "./pages/detailedcardfour";
+
 //create your first component
 const Layout = () => {
   //the basename is used when your project is published in a subdirectory and not in the root of the domain
@@ -37,6 +39,7 @@ const Layout = () => {
             <Route path="/detailedcardone" element={<Detailedcardone />} />
             <Route path="/detailedcardtwo" element={<Detailedcardtwo />} />
             <Route path="/detailedcardthree" element={<Detailedcardthree />} />
+            <Route path="/detailedcardfour" element={<Detailedcardfour />} />
             <Route path="/detailedcardsix" element={<Detailedcardsix />} />
             <Route path="/cart" element={<Checkout />} />
             <Route path="/signup" element={<Signup />} />
